c826587c29f1f20b3abaa4e503bac33a6d7c76ff
Mike <powermikee@gmail.com>
2020-08-25 03:43:45 +1000

    works in lambda

--- functions/pdf.js
+++ functions/pdf.js
@@ -2,54 +2,40 @@ import middy from "@middy/core";
 import chromium from "chrome-aws-lambda";
 import { APIGatewayEvent } from "aws-lambda";
 import doNotWaitForEmptyEventLoop from "@middy/do-not-wait-for-empty-event-loop";
+import pdf from "../lib/pdf";
 
-const handler = async (event) => {
-  const { url } = event.queryStringParameters;
-  const cookies = event.headers.Cookie.split('; ').reduce((prev, current) => {
-    const [name, value] = current.split('=');
-    prev[name] = value;
-    return prev
-  }, {});
+const handler = async (event, context) => {
+  try {
+    const { url } = event.queryStringParameters;
+    // const cookies = event.headers.Cookie.split('; ').reduce((prev, current) => {
+    //   const [name, value] = current.split('=');
+    //   prev[name] = value;
+    //   return prev
+    // }, {});
 
-  console.log(url, cookies)
+    const cookies = {
+      "pmcv-rememberme":
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImVtYWlsIjoiYWRtaW5AdXNlcmNlbnRyaWMuY29tLmF1Iiwicm9sZSI6ImFkbWluIiwiZmlyc3ROYW1lIjoiVXNlckNlbnRyaWMiLCJsYXN0TmFtZSI6IkZ1bmN0aW9uYWwgQWNjb3VudCIsInByb2Zlc3Npb24iOm51bGwsInZlcmlmaWVkIjpmYWxzZSwiaWF0IjoxNTk4Mjc3OTMzfQ.tuHVX3BUgQoukyJIICoUy3f2LlZkJlAITpYNC5NFo9U",
+    };
 
-  const executablePath = process.env.IS_OFFLINE
-    ? null
-    : await chromium.executablePath;
-    
-  const browser = await chromium.puppeteer.launch({
-    headless: true,
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath
-  });
-  const page = await browser.newPage();
+    const stream = await pdf(url, cookies);
 
-  await page.goto(url, { waitUntil: 'networkidle0' });
+    const response = {
+      statusCode: 200,
+      isBase64Encoded: true,
+      headers: {
+        'Content-type': 'application/pdf',
+        'Access-Control-Allow-Origin': '*',
+        // 'Access-Control-Allow-Credentials': true,
+        // 'Access-Control-Allow-Headers': 'Set-Cookie',
+      },
+      body: stream.toString('base64'),
+    };
 
-  const stream = await page.pdf({
-    format: 'A4',
-    margin: {
-      top: '50px',
-      left: '50px',
-      bottom: '50px',
-      right: '50px',
-    },
-    displayHeaderFooter: true,
-    headerTemplate: '<div/>',
-    footerTemplate: '<div style="font-size: 10px; font-family: arial; text-align: right; margin-right: 20px; width: 100%">Page <span class="pageNumber"></span></div>',
-  });
-
-  return {
-    statusCode: 200,
-    isBase64Encoded: true,
-    headers: {
-      "Content-type": "application/pdf",
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: stream.toString("base64")
-  };
+    return context.succeed(response);
+  } catch (error) {
+    return context.fail(error);
+  }
 };
 
 export const generate = middy(handler).use(doNotWaitForEmptyEventLoop());
