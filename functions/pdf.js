b31b519170e43f534563c3e171556cf3275d6390
Mike <powermikee@gmail.com>
2020-08-25 00:02:20 +1000

    add pdf settings

--- functions/pdf.js
+++ functions/pdf.js
@@ -4,6 +4,15 @@ import { APIGatewayEvent } from "aws-lambda";
 import doNotWaitForEmptyEventLoop from "@middy/do-not-wait-for-empty-event-loop";
 
 const handler = async (event) => {
+  const { url } = event.queryStringParameters;
+  const cookies = event.headers.Cookie.split('; ').reduce((prev, current) => {
+    const [name, value] = current.split('=');
+    prev[name] = value;
+    return prev
+  }, {});
+
+  console.log(url, cookies)
+
   const executablePath = process.env.IS_OFFLINE
     ? null
     : await chromium.executablePath;
@@ -15,9 +24,21 @@ const handler = async (event) => {
     executablePath
   });
   const page = await browser.newPage();
-  const myContent = "happy new year!!";
-  await page.setContent(myContent);
-  const stream = await page.pdf();
+
+  await page.goto(url, { waitUntil: 'networkidle0' });
+
+  const stream = await page.pdf({
+    format: 'A4',
+    margin: {
+      top: '50px',
+      left: '50px',
+      bottom: '50px',
+      right: '50px',
+    },
+    displayHeaderFooter: true,
+    headerTemplate: '<div/>',
+    footerTemplate: '<div style="font-size: 10px; font-family: arial; text-align: right; margin-right: 20px; width: 100%">Page <span class="pageNumber"></span></div>',
+  });
 
   return {
     statusCode: 200,
