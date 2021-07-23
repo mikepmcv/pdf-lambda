34e369b9cf45089524a27e5d41559728d264c3d8
Andrew Au <andrew@usercentric.com.au>
2021-07-23 15:02:57 +1000

    Add new styles for pdf redesign

--- src/lib/styles.js
+++ src/lib/styles.js
@@ -1,91 +1,230 @@
 export default `
 .pdf-visible { display: block; }
-.pmcv-notice, #_hj_feedback_container { display: none!important; }
-.ant-descriptions {
-  border: none;
-  padding: 0
+.pmcv-page-white-bg { padding-top: 0; border: none !important; }
+
+.pmcv-box {
+  border: none!important;
+  padding: 0;
+  margin-bottom: 20px;
 }
-.pdf-logo {
-  width: 100px;
-  margin-bottom: 40px;
-}
-.ant-result-success {
-  display: none;
-}
-label.pmcv-radio.ant-radio-wrapper {
-  display: none;
-}
-label.pmcv-radio.ant-radio-wrapper.ant-radio-wrapper-checked {
-  display: flex;
-}
-span.ant-radio.ant-radio-checked {
-  display: none;
-}
-.formio-component-referee_submit, .formio-component-referee_declaration { display: none;}
-.formio-component-photo {margin-bottom: 40px;}
-.ant-result-success { display: none }
-#pmcv-global-nav { display: none; }
-div.relative-position { display: none; }
+
 .pmcv-header-container, 
 .pmcv-footer-container,
+div.relative-position,
 .pmcv-admin-header,
 .pmcv-service-header,
 .pmcv-header,
+#pmcv-global-nav,
 .global-footer { display: none; }
-.form-group {
-  -webkit-print-color-adjust: exact;
-  margin-bottom: 5px;
+
+.pmcv-font-section-heading { font-size: 18px !important; }
+.pmcv-section-title h4 { display: none; }
+
+.pmcv-notice,
+#_hj_feedback_container { 
+  display: none!important;
 }
+
+.form-group.formio-component {
+  font-size: 12px;
+  line-height: normal;
+  margin-bottom: 15px;
+  padding: 0;
+}
+
+#candidate-cv-eligibility .ant-radio-wrapper-checked {
+  margin: 0;
+}
+
+#candidate-cv-referees .ant-row.mt-lg-2 {
+  margin: 0;
+}
+
+.pmcv-candidate-referees .ant-table-tbody .ant-tag.pmcv-tag { padding: 0; }
+.pmcv-candidate-referees .title-container span { font-size: 12px; }
+
+.ant-descriptions { border: none; padding: 0; }
+.ant-result-success { display: none; }
+.ant-form-item * {
+  line-height: normal;
+}
+
+.ant-form-item .ant-input.pmcv-input {
+  color: #002268;
+  font-size: 12px;
+  font-weight: 400;
+  font-style: normal;
+  line-height: normal;
+  padding: 0;
+  height: unset !important;
+  min-height: unset !important;
+}
+
 .ant-input[disabled] {
-  border: 0!important;
+  border: none!important;
   background: none!important;
   padding: 0!important;
 }
-.help-block { display: none!important; }
-.pmcv-box, .pmcv-page-white-bg {
-  border: 0!important;
+
+.ant-input::placeholder {
+  opacity: 0;
 }
+
+label.pmcv-radio.ant-radio-wrapper { display: none; }
+label.pmcv-radio.ant-radio-wrapper.ant-radio-wrapper-checked {
+  display: flex;
+}
+label.pmcv-radio.ant-radio-wrapper .ant-radio + * {
+  font-size: 12px;
+  line-height: normal;
+  padding: 0;
+}
+span.ant-radio.ant-radio-checked { display: none; }
+
+.pmcv-note,
+.pmcv-candidate-referees .ant-table-thead th:nth-child(n+3),
+.pmcv-candidate-referees .ant-table-tbody td:nth-child(n+3),
+.pmcv-candidate-referees .ant-table-body col:nth-last-child(-n+2), 
+.formio-component-submit,
+.formio-component-datagrid label:first-child,
+.formio-component-name_title,
+.formio-component-first_name,
+.formio-component-last_name,
+.formio-component-preferred_name,
+.formio-component-residentialAddressManual,
+.formio-component-gender,
+.formio-component-dob,
+.formio-component-phone_number,
+.formio-component-file.formio-component-photo,
+.formio-component-referee_submit, 
+.formio-component-referee_declaration,
+.glyphicon-question-sign,
+.help-block {
+  display: none;
+}
+
+.formio-component-photo { margin-bottom: 40px; }
+.formio-component-fieldset { margin-top: 0; }
+.formio-component-fieldset legend { font-size: 18px; }
+
+.formio-component-content {
+  color: #002268;
+  font-size: 8px;
+  font-weight: 400;
+  margin-bottom: 15px;
+  padding: 0;
+}
+
+.card-body .formio-component p, 
+.formio-component-htmlelement { 
+  color: #002268;
+  font-size: 12px;
+}
+
+.form-group {
+  -webkit-print-color-adjust: exact;
+  font-size: 12px;
+  line-height: normal;
+  margin: 0;
+  padding: 0;
+}
+.form-group .control-label {
+  color: #002268;
+  font-size: 12px;
+  margin-bottom: 0;
+}
+.form-group .control-label.field-required::after { display: none; }
+
+.form-group .choices__list { padding: 0; }
+.form-group .choices__item { height: unset; }
+
+.form-check.checkbox > label {
+  display: block;
+}
+.form-check.radio {
+  line-height: 3px;
+  height: auto !important;
+  margin: 0;
+}
+.form-check span,
+.form-check.radio span {
+  color: #002268;
+  line-height: normal;
+}
+
+.form-group .checkbox label,
+.form-group .radio label,
+.form-group .form-check-label {
+  padding-left: 0;
+}
+
+.form-check-input:not(:checked),
+.form-check-input:not(:checked) + span, 
+.referees-placeholder {
+  display: none;
+  height: 0;
+}
+
+.formio-component .form-check span,
+label.control-label.form-check-label {
+  color: #002268;
+  font-size: 12px;
+  margin-bottom: 0;
+  padding: 0!important;
+}
+
+label.control-label.form-check-label {
+  min-height: 0;
+  padding-left: 0;
+}
+
+.list-group-item:first-child,
+.list-group-item:last-child,
+.table-bordered>tbody>tr>td,
+.table-bordered>tbody>tr>th,
+.table-bordered,
+.table-bordered>tfoot>tr>td,
+.table-bordered>tfoot>tr>th,
+.table-bordered>thead>tr>td,
+.table-bordered>thead>tr>th {
+  border: 0;
+}
+
+.formio-component-datagrid table.datagrid-table thead { color: #002268; font-size: 12px; }
+.formio-component-datagrid table.datagrid-table tbody .pmcv-dg-readonly { color: #002268; margin: 0; }
+
+.datagrid-table.table>tbody>tr>td,
+.datagrid-table.table>tbody>tr>th,
+.datagrid-table.table>tfoot>tr>td,
+.datagrid-table.table>tfoot>tr>th,
+.datagrid-table.table>thead>tr>td,
+.datagrid-table.table>thead>tr>th {
+  padding: 0 4px 4px 0;
+}
+
+.formio-component-textarea div.well {
+  color: #002268;
+  font-size: 12px;
+  background: none;
+  border: none;
+  min-height: unset;
+  margin: 0;
+  padding: 0;
+  box-shadow: none;
+}
+
 .table>caption+thead>tr:first-child>td,
 .table>caption+thead>tr:first-child>th,
 .table>colgroup+thead>tr:first-child>td,
 .table>colgroup+thead>tr:first-child>th,
 .table>thead:first-child>tr:first-child>td,
 .table>thead:first-child>tr:first-child>th {
-  border-top: 0;
+  border-top: none;
   min-width: 120px;
 }
-.choices__list {
-    padding: 0!important;
-}
-.glyphicon-question-sign, .field-required:after { display: none!important;}
-.formio-component-textarea .well {
-border: 0;
-background: none;
-padding: 10px 0;
-}
-.form-check-input[disabled] {
-display: none;
-padding: 0;
-margin: 0;
-height: 0;
-}
-.form-check-input:not(:checked) + span, .referees-placeholder {
-display: none;
-height: 0;
-}
-.form-check-input + span,
-label.control-label.form-check-label {
-padding: 0!important;
-height: 0!important;
-}
-.form-check.radio {
-height: auto!important;
-line-height: 3px;
-}
-label.control-label.form-check-label {
-min-height: 0;
-}
-.list-group-item:first-child, .list-group-item:last-child, .table-bordered>tbody>tr>td, .table-bordered>tbody>tr>th, .table-bordered, .table-bordered>tfoot>tr>td, .table-bordered>tfoot>tr>th, .table-bordered>thead>tr>td, .table-bordered>thead>tr>th {
-border: 0;
+
+.formio-component.ant-form-item,
+.formio-component-datagrid {
+  break-inside: avoid;
 }
 `;
